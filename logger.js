cb2d056cfac0e12564609d6b6213a32ea448f005
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-10 20:30:24 -0800

    Fix comment

--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const EventEmitter = require('events');
 const uuid = require('uuid'); // uuid creates random ID
 
 class Logger extends EventEmitter {
-  // Every time we cal log, should show a new ID with the message that's sent
+  // Every time we call log, should show a new ID with the message that's sent
   log(msg) {
     // Call/raise an event (msg is destructuring, same as msg: msg)
     this.emit('message', { id: uuid.v4(), msg });
